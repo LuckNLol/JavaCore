c96cfb20954d881b4003966c226095d6e2e561ae
Vladimir N <savonciel@yandex.ru>
2022-10-28 10:30:54 +0300

    Cat has been changed to Car, new sout added

--- Lessons/Lesson1/Car.java
+++ Lessons/Lesson1/Car.java
@@ -5,6 +5,7 @@ public class Car {
     public void build() {
         System.out.println("You must build a new car");
         System.out.println("New branch not Cat but Car");
+        System.out.println("New branch not Cat but Car");
     }
 
 }
